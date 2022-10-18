a10d59e70ab17a1475ec794231d67b68ead27406
0xKitsune <0xKitsune@protonmail.com>
2022-10-18 01:49:45 -0400

    added all expressions to extract targets from node

--- src/parser/parse.rs
+++ src/parser/parse.rs
@@ -3,7 +3,7 @@ use std::{
     vec,
 };
 
-use solang_parser::pt::{self, Expression, Statement};
+use solang_parser::pt::{self, Expression, NamedArgument, Statement};
 
 #[derive(Eq, Hash, PartialEq, Clone, Copy)]
 pub enum Target {
@@ -458,11 +458,378 @@ pub fn extract_targets_from_node(targets: HashSet<Target>, node: Node) -> Vec<No
                     targets,
                     box_expression.into(),
                 ));
-            } // _ => {
-              //     //Address literal
-              //     //Bool literal
-              //
-              // }
+            }
+
+            pt::Expression::Delete(_, box_expression) => {
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression.into(),
+                ));
+            }
+            pt::Expression::Divide(_, box_expression, box_expression_1) => {
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression.into(),
+                ));
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression_1.into(),
+                ));
+            }
+
+            pt::Expression::Equal(_, box_expression, box_expression_1) => {
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression.into(),
+                ));
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression_1.into(),
+                ));
+            }
+
+            pt::Expression::FunctionCall(_, box_expression, vec_expression) => {
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression.into(),
+                ));
+
+                for expression in vec_expression {
+                    matches.append(&mut extract_targets_from_node(targets, expression.into()));
+                }
+            }
+
+            pt::Expression::FunctionCallBlock(_, box_expression, box_statement) => {
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression.into(),
+                ));
+
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_statement.into(),
+                ));
+            }
+
+            pt::Expression::Less(_, box_expression, box_expression_1) => {
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression.into(),
+                ));
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression_1.into(),
+                ));
+            }
+
+            pt::Expression::LessEqual(_, box_expression, box_expression_1) => {
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression.into(),
+                ));
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression_1.into(),
+                ));
+            }
+
+            pt::Expression::List(_, parameter_list) => {
+                for (_, option_parameter) in parameter_list {
+                    if option_parameter.is_some() {
+                        let parameter = option_parameter.unwrap();
+                        matches
+                            .append(&mut extract_targets_from_node(targets, parameter.ty.into()));
+                    }
+                }
+            }
+
+            pt::Expression::MemberAccess(_, box_expression, _) => {
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression.into(),
+                ));
+            }
+
+            pt::Expression::Modulo(_, box_expression, box_expression_1) => {
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression.into(),
+                ));
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression_1.into(),
+                ));
+            }
+
+            pt::Expression::More(_, box_expression, box_expression_1) => {
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression.into(),
+                ));
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression_1.into(),
+                ));
+            }
+            pt::Expression::MoreEqual(_, box_expression, box_expression_1) => {
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression.into(),
+                ));
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression_1.into(),
+                ));
+            }
+
+            pt::Expression::Multiply(_, box_expression, box_expression_1) => {
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression.into(),
+                ));
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression_1.into(),
+                ));
+            }
+
+            pt::Expression::NamedFunctionCall(_, box_expression, vec_named_argument) => {
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression.into(),
+                ));
+
+                for named_argument in vec_named_argument {
+                    matches.append(&mut extract_targets_from_node(
+                        targets,
+                        named_argument.expr.into(),
+                    ));
+                }
+            }
+
+            pt::Expression::New(_, box_expression) => {
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression.into(),
+                ));
+            }
+            pt::Expression::Not(_, box_expression) => {
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression.into(),
+                ));
+            }
+            pt::Expression::NotEqual(_, box_expression, box_expression_1) => {
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression.into(),
+                ));
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression_1.into(),
+                ));
+            }
+
+            pt::Expression::Or(_, box_expression, box_expression_1) => {
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression.into(),
+                ));
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression_1.into(),
+                ));
+            }
+
+            pt::Expression::Parenthesis(_, box_expression) => {
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression.into(),
+                ));
+            }
+
+            pt::Expression::PostDecrement(_, box_expression) => {
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression.into(),
+                ));
+            }
+
+            pt::Expression::PostIncrement(_, box_expression) => {
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression.into(),
+                ));
+            }
+
+            pt::Expression::ShiftLeft(_, box_expression, box_expression_1) => {
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression.into(),
+                ));
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression_1.into(),
+                ));
+            }
+
+            pt::Expression::ShiftRight(_, box_expression, box_expression_1) => {
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression.into(),
+                ));
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression_1.into(),
+                ));
+            }
+            pt::Expression::Subtract(_, box_expression, box_expression_1) => {
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression.into(),
+                ));
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression_1.into(),
+                ));
+            }
+
+            pt::Expression::Ternary(_, box_expression, box_expression_1, box_expression_2) => {
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression.into(),
+                ));
+
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression_1.into(),
+                ));
+
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression_2.into(),
+                ));
+            }
+            pt::Expression::Type(_, ty) => match ty {
+                pt::Type::Mapping(_, box_expression, box_expression_1) => {
+                    matches.append(&mut extract_targets_from_node(
+                        targets,
+                        box_expression.into(),
+                    ));
+
+                    matches.append(&mut extract_targets_from_node(
+                        targets,
+                        box_expression_1.into(),
+                    ));
+                }
+
+                pt::Type::Function {
+                    params,
+                    attributes,
+                    returns,
+                } => {
+                    for param in params {
+                        if param.1.is_some() {
+                            matches.append(&mut extract_targets_from_node(
+                                targets,
+                                param.1.unwrap().ty.into(),
+                            ));
+                        }
+                    }
+
+                    for attribute in attributes {
+                        match attribute {
+                            pt::FunctionAttribute::BaseOrModifier(_, base) => {
+                                if base.args.is_some() {
+                                    for arg in base.args.unwrap() {
+                                        matches.append(&mut extract_targets_from_node(
+                                            targets,
+                                            arg.into(),
+                                        ));
+                                    }
+                                }
+                            }
+
+                            pt::FunctionAttribute::NameValue(_, _, expression) => {
+                                matches.append(&mut extract_targets_from_node(
+                                    targets,
+                                    expression.into(),
+                                ));
+                            }
+                            _ => {}
+                        }
+                    }
+
+                    if returns.is_some() {
+                        let (parameter_list, function_attributes) = returns.unwrap();
+
+                        for (_, option_parameter) in parameter_list {
+                            if option_parameter.is_some() {
+                                matches.append(&mut extract_targets_from_node(
+                                    targets,
+                                    option_parameter.unwrap().ty.into(),
+                                ));
+                            }
+                        }
+
+                        for attribute in function_attributes {
+                            match attribute {
+                                pt::FunctionAttribute::BaseOrModifier(_, base) => {
+                                    if base.args.is_some() {
+                                        for arg in base.args.unwrap() {
+                                            matches.append(&mut extract_targets_from_node(
+                                                targets,
+                                                arg.into(),
+                                            ));
+                                        }
+                                    }
+                                }
+
+                                pt::FunctionAttribute::NameValue(_, _, expression) => {
+                                    matches.append(&mut extract_targets_from_node(
+                                        targets,
+                                        expression.into(),
+                                    ));
+                                }
+                                _ => {}
+                            }
+                        }
+                    }
+                }
+
+                _ => {}
+            },
+
+            pt::Expression::UnaryMinus(_, box_expression) => {
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression.into(),
+                ));
+            }
+            pt::Expression::UnaryPlus(_, box_expression) => {
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression.into(),
+                ));
+            }
+
+            pt::Expression::Unit(_, box_expression, _) => {
+                matches.append(&mut extract_targets_from_node(
+                    targets,
+                    box_expression.into(),
+                ));
+            }
+            _ => {
+                //Address literal
+                //Bool literal
+                //Hex literal
+                //Hex number literal
+                //Number literal
+                // Rational number literal
+                //String literal
+                //This
+                //Variable
+            }
         },
     }
 
