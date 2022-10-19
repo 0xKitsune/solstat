f976fb238d2cf89c6c91cfaf7a91d9a0db9d8aaa
0xKitsune <0xKitsune@protonmail.com>
2022-10-19 02:27:38 -0400

    added address balance optimization

--- src/optimizations/address_balance.rs
+++ src/optimizations/address_balance.rs
@@ -1,1 +1,61 @@
+use std::collections::HashSet;
 
+use solang_parser::pt::{self, Loc};
+use solang_parser::{self, pt::SourceUnit};
+
+use crate::ast::ast::{self, Target};
+
+pub fn address_balance_optimization(source_unit: SourceUnit) -> HashSet<Loc> {
+    let mut optimization_locations: HashSet<Loc> = HashSet::new();
+
+    let target_nodes = ast::extract_target_from_node(Target::MemberAccess, source_unit.into());
+
+    for node in target_nodes {
+        //We can use unwrap because Target::MemberAccess is an expression
+        let expression = node.expression().unwrap();
+
+        match expression {
+            pt::Expression::MemberAccess(loc, box_expression, identifier) => {
+                if let pt::Expression::FunctionCall(_, box_expression, _) = *box_expression {
+                    if let pt::Expression::Type(_, ty) = *box_expression {
+                        if let pt::Type::Address = ty {
+                            //if address(0x...).balance or address(this).balance
+                            if identifier.name == "balance".to_string() {
+                                optimization_locations.insert(loc);
+                            }
+                        }
+                    }
+                }
+            }
+
+            _ => {}
+        }
+    }
+
+    optimization_locations
+}
+
+#[test]
+fn test_address_balance_optimization() {
+    let file_contents = r#"
+    
+contract Contract0 {
+    function addressInternalBalance(){
+        uint256 bal = address(this).balance;
+        bal++;
+    }
+
+    function addressExternalBalance(address addr) public {
+        uint256 bal = address(addr).balance;
+        bal++;
+    }
+}
+
+    "#;
+
+    let source_unit = solang_parser::parse(file_contents, 0).unwrap().0;
+
+    let optimization_locations = address_balance_optimization(source_unit);
+
+    assert_eq!(optimization_locations.len(), 2)
+}
