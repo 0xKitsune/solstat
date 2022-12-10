6c4aaa0e14b4817896e48d58544436ca498ede49
Brian Le <brian@brianle.xyz>
2022-12-10 12:58:29 -0800

    refactor: add more detail about loss of precision vulnerabilities

--- src/report/report_sections/vulnerabilities/divide_before_multiply.rs
+++ src/report/report_sections/vulnerabilities/divide_before_multiply.rs
@@ -1,7 +1,7 @@
 pub fn report_section_content() -> String {
     String::from(
         r##"
-Consider ordering multiplication before division to avoid loss of precision because integer division might truncate.
+Consider ordering multiplication before division to avoid loss of precision because integer division might truncate. Loss of precision in Solidity can lead to vulnerabilities because it can result in unexpected behavior in smart contracts. This can be particularly problematic in financial applications, where even small errors in calculations can have significant consequences. For example, if a contract uses integer division to calculate a result and the division operation truncates the fractional part of the result, it could lead to incorrect pricing or loss of funds due to miscalculated balances.
 
 #### Unsafe Division
 ```js
